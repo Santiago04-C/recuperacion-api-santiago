acdb61efc889d97ad595ae0418fe7beac17332f0
Santiago04-C <santiagocanorueda8@gmail.com>
2025-12-08 14:04:01 -0500

    Limpieza de conflictos de merge

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -7,13 +6,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose', '@prisma/client']
-  }
-}
-
-module.exports = nextConfig
->>>>>>> c1aed385322b99bfb2112357b3d94fcf3aa45c01
